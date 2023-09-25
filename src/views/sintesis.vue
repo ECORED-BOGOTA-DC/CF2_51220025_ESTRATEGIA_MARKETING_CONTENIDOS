a5aac72a6927303271f02620a5db5f4f4d90de69
jorozcop <maechavarriaor@gmail.com>
2023-09-25 14:43:24 -0500

    ajustes
    ajustes

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,14 +2,16 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    p Cuando los usuarios buscan algo en la web lo primero que hacen es abrir un buscador, introducen la palabra o frase relacionada con su interés y el buscador devuelve una amplia gama de resultados, estos, vienen determinados por diversos factores, pero para las empresas, es primordial estar en las primeras páginas de los resultados y así tener mayores visitas que se transforman en mayores posibilidades de ventas. El orden en que son visualizados los resultados en el buscador es lo que determina el posicionamiento web y en la actualidad es vital para tener éxito en cualquier empresa, existe una serie de técnicas que optimizan el uso e incrementa la relevancia y visibilidad del sitio, entre las que se encuentran las estrategias de SEO y SEM pero sin dejar de lado la calidad en la gestión de los contenidos digitales a publicar. 
+
+    p.mb-5 Sabiendo esto de manera ya detallada y lo visto en el componente formativo, se presenta el siguiente esquema que resume todo esto:
 
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
           img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
       .col-auto
-        a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
+        a.anexo.mb-5(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
